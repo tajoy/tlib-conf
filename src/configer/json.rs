4b92cdb275aadd2c542fae23317979e4092c1bc0
tajoy <tj328111241@gmail.com>
2016-12-18 22:00:12 +0800

    ...

---
+++ src/configer/json.rs
@@ -0,0 +1,9 @@
+extern crate tlib_core as core;
+
+
+pub struct Json {
+}
+
+impl core::Configer for Json {
+
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 
-#![crate_name = "tlib_core"]
+#![crate_name = "tlib_conf"]
 #![crate_type = "rlib"]
 #![crate_type = "dylib"]
 #![doc(html_logo_url = "http://tajoy.net/img/logo.jpg",
@@ -8,6 +8,10 @@
        html_playground_url = "https://play.rust-lang.org/",
        test(attr(allow(unused_variables), deny(warnings))))]
 
+pub use conf::*;
+
+pub mod configer;
+mod conf;
 
 #[cfg(test)]
 mod tests {
