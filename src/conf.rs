5e2e3cfde9ac5c2ca740ae6e95ecd04e65de2f47
tajoy <tj328111241@gmail.com>
2016-12-19 22:02:29 +0800

    ...

---
+++ src/conf.rs
@@ -0,0 +1,9 @@
+
+
+pub struct Context {
+
+}
+
+impl Context {
+
+}
--- src/configer/json.rs
+++ src/configer/json.rs
@@ -1,9 +1,14 @@
 extern crate tlib_core as core;
-
+extern crate rustc_serialize as serialize;
 
 pub struct Json {
+    type Error serialize::json::DecoderError;
 }
 
 impl core::Configer for Json {
-
+    pub fn config<T: serialize::Decodable, S>(dataSource: &S) -> Result<T, Self::Error>
+        where S: str
+    {
+        // TODO
+    }
 }
